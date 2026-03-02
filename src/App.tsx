67fc63bd56577a51940fb6c0a0646dc41f403d36
muhammed salih atic <salihatic@gmail.com>
2026-03-03 00:22:31 +0300

    chore:initial project setup with vite+react+Ts

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: Salih Atiç</p>
+      <p>Ogrenci No: 230541026</p>
+    </div>
+  )
+}
+
+export default App
